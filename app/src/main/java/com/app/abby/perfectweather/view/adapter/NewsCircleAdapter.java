30aee748fdf5a0904165821df9f069019ca750d0
LitianD <948167236@qq.com>
2018-12-22 17:28:35 +0800

    增加资讯前端内容

--- app/src/main/java/com/app/abby/perfectweather/view/adapter/NewsCircleAdapter.java
+++ app/src/main/java/com/app/abby/perfectweather/view/adapter/NewsCircleAdapter.java
@@ -1,6 +1,8 @@
 package com.app.abby.perfectweather.view.adapter;
 
 import android.content.Context;
+import android.content.Intent;
+import android.print.PrintAttributes;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -9,13 +11,17 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.app.abby.perfectweather.activity.ADetailActivity;
+import com.app.abby.perfectweather.util.Resolution;
+import com.bumptech.glide.Glide;
+
 import com.app.abby.perfectweather.R;
 import com.app.abby.perfectweather.model.data.NewsBean;
 
 import java.util.List;
 
 public class NewsCircleAdapter extends RecyclerView.Adapter<NewsCircleAdapter.ViewHolder> {
-    private Context mContext;
+    private Context context;
 
     private List<NewsBean.Bean> mDatas;
     public NewsCircleAdapter(List<NewsBean.Bean> data) {
@@ -25,20 +31,47 @@ public class NewsCircleAdapter extends RecyclerView.Adapter<NewsCircleAdapter.Vi
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
 
-        if (mContext == null) {
-            mContext = parent.getContext();
+        if (context == null) {
+            context = parent.getContext();
         }
-        View view = LayoutInflater.from(mContext).inflate(R.layout.circle_of_friends,parent,false);
+        View view = LayoutInflater.from(context).inflate(R.layout.circle_of_friends,parent,false);
         return new ViewHolder(view);
 
     }
 
     @Override
     public void onBindViewHolder(ViewHolder holder,int position) {
+        final NewsBean.Bean bean = mDatas.get(position);
+        if (bean == null) {
+            return;
+        }
+        int widthPixels=context.getResources().getDisplayMetrics().widthPixels;
+        int width = widthPixels;
+        int height = Resolution.dipToPx(context, 200);
+        Glide.with(context)
+                .load(bean.getImgsrc())
+                .error(R.drawable.img_error)
+                .placeholder(R.drawable.loads)
+                .override(width,height)
+                .centerCrop()
+                .into(((ViewHolder) holder).ivNewsImg);
+        if (position == 0) {
+            ((ViewHolder) holder).tvNewsDigest.setVisibility(View.GONE);
+            ((ViewHolder) holder).tvNewsTitle.setText("图片：" + bean.getTitle());
+        } else {
+            ((ViewHolder) holder).tvNewsTitle.setText(bean.getTitle());
+            ((ViewHolder) holder).tvNewsDigest.setText(bean.getMtime() + " : " + bean.getDigest());
+            ((ViewHolder) holder).cvNews.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(context, ADetailActivity.class);
+                    intent.putExtra("url", bean.getUrl());
+                    intent.putExtra("title", bean.getTitle());
+                    context.startActivity(intent);
+                }
+            });
+        }
 
-        holder.image2.setVisibility(View.GONE);
-        holder.image3.setVisibility(View.GONE);
-        holder.textView.setText(mDatas.get(position).getLtitle());
     }
 
 
@@ -49,19 +82,19 @@ public class NewsCircleAdapter extends RecyclerView.Adapter<NewsCircleAdapter.Vi
 
 
     static class ViewHolder extends RecyclerView.ViewHolder {
-        CardView cardView;
-        ImageView image1;
-        ImageView image2;
-        ImageView image3;
-        TextView textView;
+        private ImageView ivNewsImg;
+        private TextView tvNewsTitle;
+        private TextView tvNewsDigest;
+        private CardView cvNews;
+
         public ViewHolder(View view) {
             super(view);
-            cardView = (CardView) view;
-            image1 = (ImageView) view.findViewById(R.id.friend_image1);
-            image2 = (ImageView) view.findViewById(R.id.friend_image1);
-            image3 = (ImageView) view.findViewById(R.id.friend_image1);
-            textView = (TextView) view.findViewById(R.id.friend_name);
+            ivNewsImg = (ImageView) view.findViewById(R.id.iv_news_img);
+            tvNewsTitle = (TextView) view.findViewById(R.id.tv_news_title);
+            tvNewsDigest = (TextView) view.findViewById(R.id.tv_news_digest);
+            cvNews = (CardView) view.findViewById(R.id.cv_news);
         }
+
     }
 
 
